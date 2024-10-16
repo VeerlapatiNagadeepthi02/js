8ae464d69ea9a3ae84a558b44340383695583487
VeerlapatiNagadeepthi02 <veerlapatinagadeepthi212@gmail.com>
2024-10-16 19:16:49 +0530

    msg6

--- DAY 12/image.js
+++ DAY 12/image.js
@@ -1,12 +1,18 @@
-var html =document.getElementById("HTML")
+let f=document.querySelector("img")
+let html =document.getElementById("HTML")
 html.addEventListener("click",()=>
-{
-    img.src="i"
-});
-// var css=document.createElement("CSS")
+    {
+            f.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr5kve8uJvA1JcDmKbSh1a_JFvLt5tUI8n2A&s"
+    });
+let css =document.getElementById("CSS")
+css.addEventListener("click",()=>
+        {
+                f.src="https://topskilled.in/internship/wp-content/uploads/2024/02/Vp9WvV7YKdH4k8sKRePcE8.jpg"
+        });
+let js=document.getElementById("JS")
+js.addEventListener("click",()=>
+            {
+                    f.src="https://lokalise.com/blog/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2021/10/Libraries-for-translating-JavaScript-apps.png.webp"
+            });
 
-// css.addEventListener("click",()=>
-// {
-//     img.src=""
-// });
-// var Js=document.createElement("JAVASCRIPT")
+
